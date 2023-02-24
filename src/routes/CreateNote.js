f2835d84d0f4ae33c45f73fdc53a621071f91e00
Ricardo Portatil UBUNTU <ricardovillanueva282@gmail.com>
2023-02-24 21:01:22 +0100

    mENSAJE CREATE NOTE

--- src/routes/CreateNote.js
+++ src/routes/CreateNote.js
@@ -2,16 +2,34 @@ import {useState} from "react";
 
 export default function CreateNote(){
     const [title, setTitle] = useState("");
+    const [bodyC, setBodyC] = useState("");
 
     if(localStorage.getItem("token") == null){
         window.location.replace("/");
     } else{
     
+        const fetchCreateNote = async (e) => {
+            e.preventDefault();
+            const response = await fetch("http://localhost:8081/note", {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json",
+                    "Authorization" : "Bearer " + localStorage.getItem("token")
+                },
+                body: JSON.stringify({
+                    "title" : title,
+                    "body" : bodyC
+                })
+            })
+        }
         const checkInputs = (e) => {
             if(title.length() <= 3 ){
                 alert("El titulo debe de contener mas de 3 caracteres");
+            }else{
+                fetchCreateNote(e);
             }
         }
+
         return(
             <div>
                 <h1>Crear una nueva nota</h1>
@@ -25,7 +43,9 @@ export default function CreateNote(){
                     <input type="checkbox"></input>
                 </form>
 
-                <textarea rows="7" cols="50" forum="createNoteForm" placeholder="Introduce el contenido de tu nota aqui..."></textarea>
+                <textarea rows="7" cols="50" forum="createNoteForm" placeholder="Introduce el contenido de tu nota aqui..." onChange={(v) => {
+                    setBodyC(v.target.value);
+                }}></textarea>
             </div>
         )
     
